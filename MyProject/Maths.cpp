a778aa4c13e047e2795eba9d6a762d75d140b036
Ajay Bindingnavale <ajay.bindingnavale@amd.com>
2017-08-23 13:34:53 -0500

    fix bug. Matrix was not identity..

--- MyProject/Maths.cpp
+++ MyProject/Maths.cpp
@@ -2,7 +2,7 @@
 
 glm::mat4 Maths::createModelMatrix(glm::vec3 translation, float angleX, float angleY, float angleZ, glm::vec3 scale)
 {
-	glm::mat4 modelMat;
+	glm::mat4 modelMat = glm::mat4(1);
 	modelMat = glm::translate(modelMat, translation);
 	modelMat = glm::rotate(modelMat, angleX, glm::vec3(1.0, 0.0, 0.0));
 	modelMat = glm::rotate(modelMat, angleY, glm::vec3(0.0, 1.0, 0.0));
